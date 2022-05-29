859e7b9743b847ba9ab0825c59ec1f33e6fecf94
farhanaliali <83417556+farhanaliali@users.noreply.github.com>
2022-05-29 12:24:11 +0500

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = 4001
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('Image test \n')
+  response.end('Hello World \n')
 })
 
 server.listen(port)
