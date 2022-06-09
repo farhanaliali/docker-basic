a1bb3d81ca0821703b086914593119127eebf93e
Your Name <you@example.com>
2022-06-09 21:21:24 +0500

    docker compose added

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+echo "Deploying changes..."
+# Pull changes from the live branch
+git pull
+
+# Build the image with the new changes
+docker build . -t nodeapp
+
+# Shut down the existing containers
+docker-compose down
+
+# Start the new containers
+docker-compose up -d
+echo "Deployed!"
+
