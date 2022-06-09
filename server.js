c4313ff0f54dd89557713689e3f06999c4951b50
farhanaliali <83417556+farhanaliali@users.noreply.github.com>
2022-06-09 22:05:19 +0500

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,7 @@ var port = 4001
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-<<<<<<< HEAD
-  response.end('Hello world test1 \n')
-=======
-  response.end('Hello world test 1 \n')
->>>>>>> 26b7a759f9590c7699b10e5a3fabae1ebbc544da
+    response.end('Hello from farhan \n')
 })
 
 server.listen(port)
