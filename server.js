2e64c0de58160b80413c51afcb4dfcf77d08b9f6
farhanaliali <83417556+farhanaliali@users.noreply.github.com>
2022-07-22 13:09:52 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http')
 
-var port = 4001
+var port = 4000
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
