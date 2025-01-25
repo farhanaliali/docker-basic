828ccfdb186be92925b9d615ed83086a29f61a5f
farhanaliali <83417556+farhanaliali@users.noreply.github.com>
2025-01-25 14:10:45 +0500

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ var port = 8080
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-    response.end('Hello from v4 \n')
+  response.end('Hello from v4 \n')
 })
 
-server.listen(port)
-
-console.log('Server running at http://localhost:' + port)
+// Change 'localhost' to '0.0.0.0'
+server.listen(port, '0.0.0.0', function () {
+  console.log('Server running at http://0.0.0.0:' + port)
+})
