8d09ca2b8970f02a5ff028c14ce70fa2ec874915
farhanaliali <83417556+farhanaliali@users.noreply.github.com>
2025-02-19 12:45:34 +0500

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = 8080
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('Hello from v4 \n')
+  response.end('Hello from demo-istio-v2 \n')
 })
 
 // Change 'localhost' to '0.0.0.0'
